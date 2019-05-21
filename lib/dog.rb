c16805c40aaff15dc67303639391cbc4a1fbbe18
Stella McEwen <mcewen.stella@gmail.com>
2019-05-21 17:47:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,17 @@
 class Dog 
   def name=(dog_name)
-    @this_dogs_name = dog_name 
+    @name = dog_name 
   end 
   
   def name 
-    @this_dogs_name 
+    @name 
   end 
   
   def breed=(dog_breed)
-    @this_dogs_breed = dog_breed 
+    @breed = dog_breed 
   end 
   
   def breed 
-    @this_dogs_breed 
+    @breed 
   end
 end 
